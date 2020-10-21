d5a654ab6fb0b2dc87b632e6532e82d6363fc845
usamaeltmsah <usamaeltmsah@gmail.com>
2020-10-21 14:47:35 +0200

    First IOS App :D

--- WarCardGame/ViewController.swift
+++ WarCardGame/ViewController.swift
@@ -8,12 +8,51 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
 
+    
+    @IBOutlet weak var leftImageView: UIImageView!
+    
+    @IBOutlet weak var rightImageView: UIImageView!
+    
+    @IBOutlet weak var leftScoreLabel: UILabel!
+    
+    @IBOutlet weak var rightScoreLabel: UILabel!
+    
+    var leftScore = 0
+    var rightScore = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
 
+    @IBAction func dealTapped(_ sender: Any) {
+        let leftNum = Int.random(in: 2...14)
+        let rightNum = Int.random(in: 2...14)
 
+        leftImageView.image = UIImage(named: "card\(leftNum)")
+        
+        rightImageView.image = UIImage(named: "card\(rightNum)")
+        
+        
+        if leftNum > rightNum {
+            // Left Side wins
+            leftScore += 1
+            
+            leftScoreLabel.text = String(leftScore)
+        }
+        else if rightNum > leftNum {
+            // Right Side wins
+            rightScore += 1
+            
+            rightScoreLabel.text = String(rightScore)
+        }
+        else {
+            // Tie
+        }
+    }
+    
 }
 
